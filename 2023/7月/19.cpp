a8ffbef8e3ff88c8a68a2d4568394798901c21de
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-07-19 21:20:34 +0800

    Add files via upload

---
+++ 2023/7月/19.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int removeDuplicates(int* nums, int numsSize)//https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
+{
+    int newSize = 0, a;
+    for (a = 0; a < numsSize; a++)
+    {
+        if (a == numsSize - 1)
+        {
+            nums[newSize++] = nums[a];
+            break;
+        }
+        if (nums[a] < nums[a + 1])
+        {
+            nums[newSize++] = nums[a];
+        }
+    }
+    return newSize;
+}
+
+
+
+int removeElement(int* nums, int numsSize, int val)//https://leetcode.cn/problems/remove-element/
+{
+    int newSize = 0;
+    for (int a = 0; a < numsSize; a++)
+    {
+        if (nums[a] != val)
+        {
+            nums[newSize++] = nums[a];
+        }
+    }
+    return newSize;
+}
