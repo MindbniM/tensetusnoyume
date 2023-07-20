77b1501c9bd31c599643771475ca12ae720f859c
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-07-20 10:52:56 +0800

    Add files via upload

---
+++ 2023/7月/20.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int* sortedSquares(int* nums, int numsSize, int* returnSize)//给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序
+{
+    *returnSize = numsSize;
+    int* arr = (int*)malloc(sizeof(int) * numsSize);
+    int i = 0;
+    int j = numsSize - 1;
+    int new;
+    for (new = numsSize - 1; new >= 0; new--)
+    {
+        int a = nums[i] * nums[i];
+        int b = nums[j] * nums[j];
+        if (a > b)
+        {
+            arr[new] = a;
+            i++;
+        }
+        if (a <= b)
+        {
+            arr[new] = b;
+            j--;
+        }
+    }
+    return arr;
+}
