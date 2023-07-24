2de05fc98f7d2dd8b03715284f907c8bc9fa86cd
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-07-24 22:36:34 +0800

    Add files via upload

---
+++ 2023/7月/24.cpp
@@ -0,0 +1,30 @@
+﻿#include "game.h"
+void show()
+{
+    printf("***************************\n");
+    printf("********  1.play  *********\n");
+    printf("********  0.over  *********\n");
+    printf("***************************\n");
+}
+int main()
+{
+    int a;
+    do
+    {
+        show();
+        scanf("%d", &a);
+        switch (a)
+        {
+        case 1:
+            game();
+            break;
+        case 0:
+            printf("退出\n");
+            break;
+        default:
+            printf("输入错误,重新输入");
+            break;
+        }
+    } while (a);
+    return 0;
+}
