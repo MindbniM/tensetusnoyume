259c304a3f5613561e61d655a969d6858d001f40
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-08-11 16:40:38 +0800

    Add files via upload

---
+++ 2023/8月/11.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<string.h>
+#include<math.h>
+#include <stdio.h>
+#include<stdlib.h>
+int majorityElement(int* nums, int numsSize) //求数组众数
+{
+    int i = 0;
+    int couts = nums[i];
+    int coutsnum = 1;
+    for (i = 1; i < numsSize; i++)
+    {
+        if (coutsnum == 0)
+        {
+            couts = nums[i];
+        }
+        if (couts == nums[i])
+            coutsnum++;
+        else
+            coutsnum--;
+    }
+    return couts;
+}
