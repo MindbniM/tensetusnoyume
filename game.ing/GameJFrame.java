c5299a79290d7a2423916b30fee2ffd672a08c89
lufangquan <118365358+MindbniM@users.noreply.github.com>
2023-09-22 16:25:52 +0800

    Add files via upload

---
+++ game.ing/GameJFrame.java
@@ -0,0 +1,10 @@
+package com.MindbniM.ui;
+
+import javax.swing.*;
+
+public class GameJFrame extends JFrame {
+    public GameJFrame(){
+        this.setSize(720,480);
+        this.setVisible(true);
+    }
+}
