a1082f7b967df04722a6a59b2331234cdd8fe722
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-09-26 21:47:12 +0800

    Add files via upload

--- game.ing/GameJFrame.java
+++ game.ing/GameJFrame.java
@@ -5,6 +5,14 @@ import javax.swing.*;
 public class GameJFrame extends JFrame {
     public GameJFrame(){
         this.setSize(720,480);
+        initimage();
         this.setVisible(true);
+    }
+
+    private  void initimage() {
+        ImageIcon image=new ImageIcon("C:\\Users\\17713\\IdeaProjects\\game\\huanjie\\mmexport1695718070288.png");
+        JLabel J=new JLabel(image);
+        J.setBounds(0,0,108,108);
+        this.getContentPane().add(J);
     }
 }
