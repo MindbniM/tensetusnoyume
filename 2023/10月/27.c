dab0ef0570dc2087c119acf85b53cf9ae4665749
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-10-27 22:11:01 +0800

    Add files via upload

--- 2023/10月/27.c
+++ 2023/10月/27.c
@@ -5,14 +5,73 @@
 #include<assert.h>
 #include<ctype.h>
 #include <stdio.h>
-typedef int typed;
+typedef int ElenType;
 typedef struct Tree
 {
-	typed date;
+	ElenType date;
 	struct Tree* lchild;
 	struct Tree* rchild;
 }Tree;
-Tree* createTree(typed date)
+typedef struct Node
+{
+	Tree* date;
+	struct Node* top;
+}Node;
+typedef struct Stack
+{
+	struct Node* Node;
+	int num;
+}Stack;
+Stack* InitStack()
+{
+	Stack* S =(Stack*) malloc(sizeof(Stack));
+	if (S == NULL)
+	{
+		perror("malloc:");
+		return NULL;
+	}
+	S->num = 0;
+	S->Node = NULL;
+	return S;
+}
+void push(Stack* St, Tree* data)
+{
+	if (St == NULL)
+		return ;
+	Node* S = (Node*)malloc(sizeof(Node));
+	if (S == NULL)
+	{
+		perror("malloc:");
+		return ;
+	}
+	Node* p = St->Node;
+	St->Node = S;
+	S->date = data;
+	S->top = p;
+	St->num++;
+}
+Tree* pop(Stack* St)
+{
+	if (St == NULL)
+		return NULL;
+	Node* p = St->Node;
+	Tree* num = p->date;
+	St->Node = St->Node->top;
+	free(p);
+	St->num--;
+	return num;
+}
+void freeStack(Stack* St)
+{
+	if (St == NULL)
+		return ;
+	while (St->Node != NULL)
+	{
+		pop(St);
+	}
+	free(St);
+}
+Tree* createTree(ElenType date)
 {
 	Tree* T = (Tree*)malloc(sizeof(Tree));
 	if (T == NULL)
@@ -24,6 +83,26 @@ Tree* createTree(typed date)
 	T->lchild = NULL;
 	T->rchild = NULL;
 	return T;
+}
+void Stack_print_Tree(Stack* St, Tree* T)
+{
+	if (St == NULL||T==NULL)
+		return ;
+	Tree* t = T;
+	while (t != NULL || St->num > 0)
+	{
+		if (t != NULL)
+		{
+			push(St, t);
+			t = t->lchild;
+		}
+		else
+		{
+			Tree* Tr = pop(St);
+			printf("%d", Tr->date);
+			t = Tr->rchild;
+		}
+	}
 }
 void print_Tree(Tree* T)
 {
@@ -51,6 +130,7 @@ void freeTree(Tree* T)
 }
 int main()
 {
+	Stack* S = InitStack();
 	Tree* T = createTree(1);
 	T->lchild = createTree(2);
 	T->rchild = createTree(3);
@@ -58,7 +138,10 @@ int main()
 	T->lchild->rchild = createTree(5);
 	T->rchild->lchild = createTree(6);
 	T->rchild->rchild = createTree(7);
+	Stack_print_Tree(S, T);
+	printf("\n");
 	print_Tree(T);
 	freeTree(T);
+	T = NULL;
 	return 0;
 }
