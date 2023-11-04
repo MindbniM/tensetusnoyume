0fd992c2bc56d1eb4063d84859ca1dde9505d0f5
MindbniM <118365358+MindbniM@users.noreply.github.com>
2023-11-04 23:39:04 +0800

    Add files via upload

--- 2023/10月/4.c
+++ 2023/10月/4.c
@@ -1,110 +1,174 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<time.h>
-#include<math.h>
-#include<string.h>
-#include<stdlib.h>
-#include<assert.h>
-#include<ctype.h>
-#include <stdio.h>
-//typedef struct tim
-//{
-//    int arr;
-//    int date;
-//}ti;
-//int comp(const void* arr1, const void* arr2)
-//{
-//    int s1 = abs(((ti*)arr1)->date);
-//    int s2 = abs(((ti*)arr2)->date);
-//    if (((ti*)arr1)->date < 0 && s1 == s2)
-//        return -1;
-//    if (((ti*)arr2)->date < 0 && s1 == s2)
-//        return 1;
-//    return s1 - s2;
-//}
-//int sort(int Date)
-//{
-//    int Year = Date / 10000;
-//    int Month = (Date % 10000) / 100;
-//    int Day = Date % 100;
-//    struct tm date = { 0, 0, 0, Day, Month - 1, Year - 1900 };
-//    struct tm date1 = { 0, 0, 0, 19, 10, 122 };
-//    time_t time = mktime(&date);
-//    time_t now = mktime(&date1);
-//    double tomp = difftime(now, time) / (24 * 60 * 60);
-//    return (int)tomp;
-//}
-//int main()
-//{
-//    int n;
-//    scanf("%d", &n);
-//    ti arr[10];
-//    int num;
-//    for (int i = 0; i < n; i++)
-//    {
-//        scanf("%d", &num);
-//        arr[i].arr = num;
-//        arr[i].date = sort(num);
-//    }
-//    qsort(arr, n, sizeof(ti), comp);
-//    for (int i = 0; i < n; i++)
-//    {
-//        printf("%08d\n", arr[i].arr);
-//    }
-//}
-int comp(int (*arr)[9], int i, int j, int c)
+#include "text.h"
+SListNode* BuySListNode(SLTDateType x)
 {
-    int arr1[10] = { 0 };
-    if (c)
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            arr1[arr[i][j]] += 1;
-        }
-        for (int i = 1; i < 9; i++)
-        {
-            if (arr1[i] == 0)
-                return 0;
-        }
-        return 1;
-    }
-    else
-    {
-        for (int j = 0; j < 9; j++)
-        {
-            arr1[arr[i][j]] += 1;
-        }
-        for (int i = 1; i < 9; i++)
-        {
-            if (arr1[i] == 0)
-                return 0;
-        }
-        return 1;
-    }
+	SListNode* newNode = (SListNode*)malloc(sizeof(SListNode));
+	if (newNode == NULL)
+	{
+		perror("malloc:");
+		exit(-1);
+	}
+	newNode->data = x;
+	newNode->next = NULL;
+	return newNode;
+}
+void SListPrint(SListNode* plist)
+{
+	if (plist == NULL)
+	{
+		printf("NULL");
+	}
+	else
+	{
+		while (plist != NULL)
+		{
+			printf("%d ", plist->data);
+			plist = plist->next;
+		}
+	}
+	printf("\n");
+}
+void SListFree(SListNode** pplist)
+{
+	assert(pplist!=NULL);
+	SListNode* p = *pplist;
+	while (p != NULL)
+	{
+		SListNode* pc = p;
+		p = p->next;
+		free(pc);
+	}
+	*pplist = NULL;
+}
+void SListPushFront(SListNode** pplist, SLTDateType x)
+{
+	assert(pplist != NULL);
+	if (*pplist == NULL)
+	{
+		SListNode* newNode = BuySListNode(x);
+		*pplist = newNode;
+	}
+	else
+	{
+		SListNode* newNode = BuySListNode(x);
+		SListNode* p = *pplist;
+		*pplist = newNode;
+		newNode->next = p;
+	}
+}
+void SListPushBack(SListNode** pplist, SLTDateType x)
+{
+	assert(pplist != NULL);
+	SListNode* p = *pplist;
+	if (p == NULL)
+	{
+		SListNode* newNode = BuySListNode(x);
+		*pplist = newNode;
+		return;
+	}
+	while (p ->next!= NULL)
+	{
+		p = p->next;
+	}
+	SListNode* newNode = BuySListNode(x);
+	p->next = newNode;
+
+}
+void SListPopBack(SListNode** pplist)
+{
+	assert(pplist != NULL&&*pplist!=NULL);
+	SListNode* p = *pplist;
+	if (p->next == NULL)
+	{
+		free(*pplist);
+		*pplist = NULL;
+	}
+	else
+	{
+		while (p->next->next != NULL)
+		{
+			p = p->next;
+		}
+		free(p->next);
+		p->next = NULL;
+	}
+}
+void SListPopFront(SListNode** pplist)
+{
+	assert(pplist != NULL&&*pplist!=NULL);
+	SListNode* p = *pplist;
+	if (p->next == NULL)
+	{
+		free(*pplist);
+		*pplist = NULL;
+	}
+	else
+	{
+		*pplist = p->next;
+		free(p);
+	}
+}
+SListNode* SListFind(SListNode* plist, SLTDateType x)
+{
+	assert(plist != NULL );
+	while (plist != NULL)
+	{
+		if (plist->data == x)
+			return plist;
+		plist = plist->next;
+	}
+	return NULL;
+}
+void SListInsertAfter(SListNode* pow, SLTDateType x)
+{
+	if (pow == NULL)
+	{
+		printf("该结点不存在\n");
+		return;
+	}
+	SListNode* newNode = BuySListNode(x);
+	SListNode*p=pow->next;
+	pow->next = newNode;
+	newNode->next = p;
+}
+void SListEraseAfter(SListNode* pow)
+{
+	if (pow == NULL)
+	{
+		printf("该结点不存在\n");
+		return;
+	}
+	SListNode* p = NULL;
+	if (pow->next != NULL)
+	{
+		p = pow->next->next;
+	}
+	free(pow->next);
+	pow->next = p;
+}
+SListNode* SLTInsert(SListNode** pphead, SListNode* pow, SLTDateType x)
+{
+
+}
+void text1()
+{
+	SListNode* head = BuySListNode(1);
+	head->next = BuySListNode(2);
+	head->next->next = BuySListNode(3);
+	head->next->next->next = BuySListNode(4);
+	//SListPushFront(&head,1);
+	//SListPushBack(&head, 3);
+	//SListPrint(head);
+	//SListPopBack(&head);
+	//SListPopFront(&head);
+	SListInsertAfter(SListFind(head, 4), 5);
+	SListPrint(head);
+	SListEraseAfter(SListFind(head, 6));
+	SListPrint(head);
+	SListFree(&head);
 }
 int main()
 {
-    int arr[9][9];
-    int a, b;
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 9; j++)
-        {
-            scanf("%d", &arr[i][j]);
-            if (!arr[i][j])
-            {
-                a = i, b = j;
-            }
-        }
-    }
-    int num = 0, sum = 0;
-    for (int i = 0; i < 9; i++)
-    {
-        num += arr[a][i];
-        sum += arr[i][b];
-    }
-    arr[a][b] = 45 - num;
-    if (num == sum && comp(arr, a, b, 1) && comp(arr, a, b, 0))
-        printf("%d", 45 - num);
-    else
-        printf("NO");
+	text1();
+	return 0;
 }
