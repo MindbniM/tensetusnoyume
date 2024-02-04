bebf8c5c2c1513a700b2eee0b597c2e9cd079684
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-02-04 20:21:57 +0800

    Add files via upload

--- 2024/2.4/text.cpp
+++ 2024/2.4/text.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//using namespace std;
-#include"Data.h"
-
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int hash[128] = { 0 };
+        int hashs[128] = { 0 };
+        vector<string> vs;
+        for (auto c : t)
+            hash[c]++;
+        for (int dest = 0, cur = 0, cou = 0; cur < s.size(); cur++)
+        {
+            hashs[s[cur]]++;
+            if (hashs[s[cur]] <= hash[s[cur]])
+                cou++;
+            while (cou == t.size())
+            {
+                vs.push_back(s.substr(dest, cur - dest+1));
+                if (hashs[s[dest]] <= hash[s[dest]])
+                    cou--;
+                hashs[s[dest]]--;
+                dest++;
+            }
+        }
+        int m = INT_MAX;
+        int ret = -1;
+        for (int i = 0; i < vs.size(); i++)
+        {
+            if (m > vs[i].size())
+            {
+                ret = i;
+                m = vs[i].size();
+            }
+        }
+        return vs[ret];
+    }
+};
 int main()
 {
-	MindbniM::Time t(2024, 2, 4, 12);
-	Data& d = t;
-	std::cout << d;
+    string s=Solution().minWindow("ADOBECODEBANC","ABC");
 	return 0;
 }
