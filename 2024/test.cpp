82b2fc08a8668509cf37c7363e430f1b2b3765e7
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-04-04 20:11:02 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,8 +1,26 @@
-#include"str.h"
+#include<iostream>
+#include<vector>
+#include<utility>
+#include"hash_bucket.h"
+#include"unordered_set.h"
+#include"unordered_map.h"
 int main()
 {
-	M::string s2;
-	cin >> s2;
-	cout << s2 << endl;
+	unordered_map<int,int> hash;
+	hash[4] = 1;
+	hash[14] = 7;
+	hash[5] = 6;
+	hash[3] = 3;
+	hash[8] = 0;
+	unordered_map<int,int>::iterator it = hash.begin();
+	/*while (it != hash.end())
+	{
+		std::cout << (*it).first << " "<<(*it).second<<std::endl;
+		it = hash.erase((*it).first);
+	}*/
+	for (auto& p : hash)
+	{
+		std::cout << p.first << " " << p.second << std::endl;
+	}
 	return 0;
 }
