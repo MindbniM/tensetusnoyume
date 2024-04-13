1b3be0522b5b5da45a050ac41d30c594d11b33b6
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-04-13 22:02:25 +0800

    Add files via upload

---
+++ 2024/list.h
@@ -0,0 +1,206 @@
+#pragma once
+#include"Reverse_iterator.h"
+namespace M
+{
+	template<class T>
+	struct ListNode
+	{
+		ListNode<T>* _prev;
+		ListNode<T>* _next;
+		T _val;
+		ListNode(const T& val=T())
+			:_prev(nullptr)
+			,_next(nullptr)
+			,_val(val)
+		{
+		}
+	};
+
+	template<class T,class CIT,class PTR>
+	struct List_iterator
+	{
+		typedef ListNode<T> Node;
+		typedef List_iterator<T,CIT,PTR> self;
+		Node* _node;
+		List_iterator(Node* node)
+			:_node(node)
+		{
+		}
+		List_iterator(const self& s)
+		{
+			_node = s._node;
+		}
+		self& operator++()
+		{
+			_node = _node->_next;
+			return *this;
+		}
+		self& operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+		CIT operator*() const
+		{
+			return _node->_val;
+		}
+		bool operator!=( const self& li) const
+		{
+			return _node != li._node;
+		}
+		bool operator==(const self& li) const
+		{
+			return _node == li._node;
+		}
+		PTR operator->() const
+		{
+			return &_node->_val;
+		}
+	};
+
+	template<class T>
+	class list
+	{
+		typedef ListNode<T> Node;
+	public:
+		typedef List_iterator<T,T&,T*> iterator;
+		typedef List_iterator<T,const T&,const T*> const_iterator;
+		typedef Reverse_iterator<iterator, T&, T*> reverse_iterator;
+		typedef Reverse_iterator<const_iterator,const T&, const T*> const_reverse_iterator;
+
+		list()
+		{
+			empty_init();
+		}
+		void empty_init()
+		{
+			_head = new ListNode<T>();
+			_head->_next = _head;
+			_head->_prev = _head;
+		}
+		list(const list<T>& li)
+		{
+			empty_init();
+			for (auto& l : li)
+			{
+				push_back(l);
+			}
+		}
+		~list()
+		{
+			clear();
+			delete _head;
+			_head = nullptr;
+		}
+		void push_back(const T& val)
+		{
+			insert(end(), val);
+		}
+		void push_front(const T& val)
+		{
+			insert(begin(), val);
+		}
+		void pop_back()
+		{
+			erase(--end());
+		}
+		void pop_front()
+		{
+			erase(begin());
+		}
+		iterator insert(iterator it,const T& val)
+		{
+			Node* prev = it._node->_prev;
+			Node* node = it._node;
+			Node* newnode = new Node(val);
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = node;
+			node->_prev = newnode;
+			return newnode;
+		}
+		iterator erase(iterator it)
+		{
+			Node* node = it._node;
+			Node* next = node->_next;
+			node->_prev->_next = next;
+			next->_prev = node->_prev;
+			delete node;
+			node = nullptr;
+			return next;
+		}
+		iterator begin()
+		{
+			return _head->_next;
+		}
+		iterator end()
+		{
+			return _head;
+		}
+		const_iterator begin() const
+		{
+			return _head->_next;
+		}
+		const_iterator end() const
+		{
+			return _head;
+		}
+		reverse_iterator rbegin()
+		{
+			return reverse_iterator(end());
+		}
+		reverse_iterator rend()
+		{
+			return reverse_iterator(begin());
+		}
+		const_reverse_iterator crbegin() const
+		{
+			return const_reverse_iterator(end());
+		}
+		const_reverse_iterator crend() const
+		{
+			return const_reverse_iterator(begin());
+		}
+		void clear()
+		{
+			iterator b = begin();
+			while (b != end())
+			{
+				b=erase(b);
+			}
+		}
+		void swap(list& li)
+		{
+			std::swap(_head, li._head);
+		}
+		void resize(size_t n, const T& val = T())
+		{
+			while (n--)
+			{
+				push_back(val);
+			}
+		}
+		bool empty()
+		{
+			return _head->_next == _head;
+		}
+		int size()
+		{
+			iterator it = begin();
+			int n = 0;
+			while (it != end())
+			{
+				++it;
+				n++;
+			}
+			return n;
+		}
+		list& operator=(list li)
+		{
+			swap(li);
+			return *this;
+		}
+	private:
+		Node* _head;
+	};
+}
