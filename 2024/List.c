4a87a931191f9c49bced5ff41629617a772871f5
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-04-16 22:26:57 +0800

    Add files via upload

---
+++ 2024/List.c
@@ -0,0 +1,124 @@
+#include"STL.h"
+ListNode* NodeCreate(LTDataType x)
+{
+	ListNode* Node = (ListNode*)malloc(sizeof(ListNode));
+	if (Node == NULL)
+	{
+		perror("malloc;");
+		exit(-1);
+	}
+	Node->next = NULL;
+	Node->prev = NULL;
+	Node->data = x;
+	return Node;
+}
+ListNode* ListCreate()
+{
+	ListNode* head = (ListNode*)malloc(sizeof(ListNode));
+	if (head == NULL)
+	{
+		perror("malloc;");
+		return NULL;
+	}
+	head->data = 0;
+	head->next = head;
+	head->prev = head;
+	return head;
+}
+void ListDestory(ListNode* pHead)
+{
+	assert(pHead);
+	ListNode* pHeadnext = pHead->next;
+	while (pHeadnext != pHead)
+	{
+		ListNode* p = pHeadnext;
+		pHeadnext = pHeadnext->next;
+		free(p);
+	}
+	free(pHead);
+}
+void ListPrint(ListNode* pHead)
+{
+	assert(pHead);
+	ListNode* pHeadnext = pHead->next;
+	printf("head");
+	while (pHeadnext != pHead)
+	{
+		ListNode* p = pHeadnext;
+		pHeadnext = pHeadnext->next;
+		printf("<-->%d", p->data);
+	}
+	printf("\n");
+}
+void ListPushBack(ListNode* pHead, LTDataType x)
+{
+	assert(pHead);
+	ListNode* newNode = NodeCreate(x);
+	newNode->prev = pHead->prev;
+	pHead->prev->next = newNode;
+	newNode->next = pHead;
+	pHead->prev = newNode;
+}
+void ListPopBack(ListNode* pHead)
+{
+	assert(pHead);
+	ListNode* pHeadprev = pHead->prev;
+	if (pHeadprev == pHead)
+		return;
+	pHead->prev = pHeadprev->prev;
+	pHeadprev->prev->next = pHead;
+	free(pHeadprev);
+	pHeadprev = NULL;
+}
+void ListPushFront(ListNode* pHead, LTDataType x)
+{
+	assert(pHead);
+	ListNode* newNode = NodeCreate(x);
+	ListNode* pHeadnext = pHead->next;
+	pHead->next = newNode;
+	newNode->prev = pHead;
+	newNode->next = pHeadnext;
+	pHeadnext->prev = newNode;
+}
+void ListPopFront(ListNode* pHead)
+{
+	assert(pHead);
+	ListNode* pHeadnext = pHead->next;
+	if (pHeadnext == pHead)
+		return;
+	pHead->next = pHeadnext->next;
+	pHeadnext->next->prev = pHead;
+	free(pHeadnext);
+	pHeadnext = NULL;
+}
+ListNode* ListFind(ListNode* pHead, LTDataType x)
+{
+	assert(pHead);
+	ListNode* pHeadnext = pHead->next;
+	while (pHeadnext != pHead)
+	{
+		if (pHeadnext->data == x)
+			return pHeadnext;
+		pHeadnext = pHeadnext->next;
+	}
+	return NULL;
+}
+void ListInsert(ListNode* pos, LTDataType x)
+{
+	assert(pos);
+	ListNode* posprev = pos->prev;
+	ListNode* newNode = NodeCreate(x);
+	posprev->next = newNode;
+	newNode->prev = posprev;
+	pos->prev = newNode;
+	newNode->next = pos;
+}
+void ListErase(ListNode* pos)
+{
+	assert(pos);
+	ListNode* posprev = pos->prev;
+	posprev->next = pos->next;
+	pos->next->prev = posprev;
+	free(pos);
+	pos = NULL;
+}
