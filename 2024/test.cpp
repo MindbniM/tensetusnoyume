7c9752541028208e735d573a23a30983c5e139d1
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-05-13 23:24:35 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,49 +1,14 @@
 ﻿#include<iostream>
-#include<unordered_set>
-#include<vector>
-#include<string>
+#include<functional>
 using namespace std;
+template<class T>
+void fun(function<T> f)
+{
+	f();
+}
 int main()
 {
-    int n;
-    cin >> n;
-    for (int o = 0; o < n; o++)
-    {
-        int m;
-        cin >> m;
-        vector<string> vs(m);
-        for (int i = 0; i < m; i++)
-        {
-            cin >> vs[i];
-        }
-        unordered_set<char> hash;
-        int i = 0;
-        m--;
-        while (i != m && i != m - 1)
-        {
-            hash.clear();
-            int coun = 0;
-            for (auto c : vs[i]) hash.insert(c);
-            for (auto c : vs[m])
-            {
-                if (hash.count(c))
-                {
-                    coun = 1;
-                    break;
-                }
-            }
-            if (coun == 0)
-            {
-                cout << "No" << endl;
-                break;
-            }
-            i++;
-            m--;
-        }
-        if (i == m || i == m - 1)
-        {
-            cout << "Yes" << endl;
-        }
-    }
-    return 0;
+	fun<void()>([] {cout << "000"; });
+	void(*p)(int) = nullptr;
+	return 0;
 }
