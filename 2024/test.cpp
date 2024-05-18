eeb168277c9c1886fc98de384c2bbc0b50b3442a
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-05-18 23:04:31 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,14 +1,26 @@
-﻿#include<iostream>
-#include<functional>
-using namespace std;
-template<class T>
-void fun(function<T> f)
-{
-	f();
-}
+#include<iostream>
+#include<vector>
+#include<utility>
+#include"hash_bucket.h"
+#include"unordered_set.h"
+#include"unordered_map.h"
 int main()
 {
-	fun<void()>([] {cout << "000"; });
-	void(*p)(int) = nullptr;
+	unordered_map<int,int> hash;
+	hash[4] = 1;
+	hash[14] = 7;
+	hash[5] = 6;
+	hash[3] = 3;
+	hash[8] = 0;
+	unordered_map<int,int>::iterator it = hash.begin();
+	/*while (it != hash.end())
+	{
+		std::cout << (*it).first << " "<<(*it).second<<std::endl;
+		it = hash.erase((*it).first);
+	}*/
+	for (auto& p : hash)
+	{
+		std::cout << p.first << " " << p.second << std::endl;
+	}
 	return 0;
 }
