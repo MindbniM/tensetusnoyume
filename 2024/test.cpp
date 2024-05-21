e3e0b5bdda171a8b3160854be50d2f67c9ddd557
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-05-21 22:26:23 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,9 +1,20 @@
-﻿#include<iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<cstdio>
+#include<cstring>
 using namespace std;
 int main()
 {
-	string str = "222";
-	string s = "111" + str;
-	cout << s;
+	FILE* r=fopen("111.txt", "r");
+	FILE* w = fopen("data.txt", "w");
+	double n = 10;
+	for (n = 10; n <= 80; n += 0.4)
+	{
+		char arr[10] = { 0 };
+		fscanf(r, "%s", arr);
+		fprintf(w, "%.1llf    %s\n", n, arr);
+	}
+	fclose(r);
+	fclose(w);
 	return 0;
 }
