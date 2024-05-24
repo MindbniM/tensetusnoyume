f04d07c583f1092489a33adfd1112d05b25f858c
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-05-24 22:53:49 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -7,13 +7,35 @@ int main()
 {
 	FILE* r=fopen("111.txt", "r");
 	FILE* w = fopen("data.txt", "w");
-	double n = 10;
-	for (n = 10; n <= 80; n += 0.4)
+	double n ;
+	for (n = -3; n <= 3; n += 0.5)
 	{
 		char arr[10] = { 0 };
+		char arr1[10] = { 0 };
+		char arr2[10] = { 0 };
 		fscanf(r, "%s", arr);
-		fprintf(w, "%.1llf    %s\n", n, arr);
+		fscanf(r, "%s", arr1);
+		fscanf(r, "%s", arr2);
+		fprintf(w, "%.1llf       %s       %s       %s\n", n, arr,arr1,arr2);
 	}
+	for (n = 5; n <= 29; n += 2)
+	{
+		char arr[10] = { 0 };
+		char arr1[10] = { 0 };
+		char arr2[10] = { 0 };
+		fscanf(r, "%s", arr);
+		fscanf(r, "%s", arr1);
+		fscanf(r, "%s", arr2);
+		fprintf(w, "%.1llf       %s       %s       %s\n", n, arr,arr1,arr2);
+	}
+	n = 30;
+	char arr[10] = { 0 };
+	char arr1[10] = { 0 };
+	char arr2[10] = { 0 };
+	fscanf(r, "%s", arr);
+	fscanf(r, "%s", arr1);
+	fscanf(r, "%s", arr2);
+	fprintf(w, "%.1llf       %s       %s       %s\n", n, arr,arr1,arr2);
 	fclose(r);
 	fclose(w);
 	return 0;
