5b393442cd8592adbd5f391f6b631637b7a4f80e
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-07-26 20:11:35 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -6,26 +6,11 @@
 #include<iostream>
 #include<memory>
 using namespace std;
-template<class T,class index>
-decltype(auto) at( T&& v, index n)
-{
-	return forward<T>(v)[n];
-}
-template<typename Container, typename Index>    //最终的C++11版本
-auto authAndAccess(Container&& c, Index i) -> decltype(std::forward<Container>(c)[i])
-{
-	authenticateUser();
-	return std::forward<Container>(c)[i];
-}
-
 int main()
 {
-	vector<int> v = { 1,2,4 };
-	at(v, 2) = 1;
-	for (auto& i : v)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
+	bool a=true;
+	bool& b = a;
+	b = false;
+	cout << a << endl;
 	return 0;
 }
