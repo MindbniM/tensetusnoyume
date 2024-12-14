d6bbf2ddacb52f2ef3dbdeaba03b5e8c87af217d
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-12-14 21:17:05 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,21 +1,34 @@
-import <iostream>;
-#include<thread>
-#include<functional>
-#include<chrono>
-#include<future>
+#include<iostream>
+#include<queue>
 using namespace std;
-int test()
+long long gcd(long long a, long long b)
 {
-	return 1;
+	while (a != b)
+	{
+		if (a > b) a = a - b;
+		else b = b - a;
+	}
+	return a;
 }
+long long arr[100001];
 int main()
 {
-	std::packaged_task<int()> task(test);
-	thread t([&task]
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	int m = INT_MAX;
+	for (int i = 1; i < n; i++)
+	{
+		long long n1 = arr[i - 1], n2 = arr[i];
+		while (n1 != 1 && n2 != 1)
 		{
-			task();
-		});
-	t.join();
-	cout << task.get_future().get() << endl;
+			gcd(n1, n2);
+		}
+	}
 	return 0;
+
 }
