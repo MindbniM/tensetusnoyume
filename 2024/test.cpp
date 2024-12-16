d43623d722f59da809e6214766836e4ed6b4d5c5
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-12-16 20:51:34 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,34 +1,37 @@
 #include<iostream>
-#include<queue>
+#include<string>
+#include<vector>
+#include<algorithm>
 using namespace std;
-long long gcd(long long a, long long b)
+vector<long long> v;
+bool isNum(long long num)
 {
-	while (a != b)
-	{
-		if (a > b) a = a - b;
-		else b = b - a;
-	}
-	return a;
+	long long n = sqrt(num);
+	return n * n == num;
 }
-long long arr[100001];
+bool dfs(const string& str,int n,int num)
+{
+	if (n == str.size()&&num==2) return true;
+	for (int i = n; i < str.size(); i++)
+	{
+		long long temp = stoll(str.substr(n, i - n + 1));
+		if(temp!=0&&isNum(temp)&&dfs(str,i+1,num+1)) return true;
+	}
+	return false;
+}
 int main()
 {
-
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	long long a,b;
+	cin >> a >> b;
+	for (long long i = sqrt(a); i < sqrt(b); i++)
 	{
-		cin >> arr[i];
-	}
-	int m = INT_MAX;
-	for (int i = 1; i < n; i++)
-	{
-		long long n1 = arr[i - 1], n2 = arr[i];
-		while (n1 != 1 && n2 != 1)
+		long long temp = i * i;
+		if (isNum(temp))
 		{
-			gcd(n1, n2);
+			string str = to_string(temp);
+			if(dfs(str,0,0))
+			cout << temp << endl;
 		}
 	}
 	return 0;
-
 }
