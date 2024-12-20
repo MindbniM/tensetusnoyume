13949306e08a00232139c7fe97c0fa06a9e6aab2
MindbniM <118365358+MindbniM@users.noreply.github.com>
2024-12-20 22:10:14 +0800

    Add files via upload

--- 2024/test.cpp
+++ 2024/test.cpp
@@ -1,37 +1,44 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include <iostream>
 using namespace std;
-vector<long long> v;
-bool isNum(long long num)
-{
-	long long n = sqrt(num);
-	return n * n == num;
-}
-bool dfs(const string& str,int n,int num)
-{
-	if (n == str.size()&&num==2) return true;
-	for (int i = n; i < str.size(); i++)
-	{
-		long long temp = stoll(str.substr(n, i - n + 1));
-		if(temp!=0&&isNum(temp)&&dfs(str,i+1,num+1)) return true;
-	}
-	return false;
-}
+long long v[10];
+long long sum[10];
+int h1[10] = { 0,1,1,2,2,3,3,4,4,5 };
+int h2[10] = { 0,0,1,1,2,2,3,3,4,4 };
 int main()
 {
-	long long a,b;
-	cin >> a >> b;
-	for (long long i = sqrt(a); i < sqrt(b); i++)
+	ios::sync_with_stdio(0);
+	string str;
+	cin >> str;
+	v[0] = 1;
+	sum[0] = 0;
+	int n = str.size();
+	for (int i = 1; i < n; i++)
 	{
-		long long temp = i * i;
-		if (isNum(temp))
+		v[i] = v[i - 1] * 5;
+		sum[i] = sum[i - 1];
+		if (i % 2 == 0) sum[i] += v[i - 1] * 4;
+		else sum[i] += v[i];
+	}
+	long long num = 0;
+	if (n > 1) num = sum[n - 1];
+	//cout << num << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if ((n - i) % 2 == 0)
 		{
-			string str = to_string(temp);
-			if(dfs(str,0,0))
-			cout << temp << endl;
+			int temp=h1[str[i] - '0'];
+			if (i == 0) temp--;
+			num += temp * v[n - i - 1];
+			if ((str[i]-'0') % 2 != 0) break;
+		}
+		else
+		{
+			int temp=h2[str[i] - '0'];
+			num += temp * v[n - i - 1];
+			if (i==n-1&&(str[i] - '0') % 2 == 1) num++;
+			if ((str[i]-'0') % 2 != 1) break;
 		}
 	}
+	cout << num;
 	return 0;
 }
